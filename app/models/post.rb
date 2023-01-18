3e2b8db5334ce9e21bc6bac62847b5cedd7c7b24
Diego Mero <diegomero_1999@hotmail.com>
2023-01-18 01:34:57 -0500

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentsCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likesCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
