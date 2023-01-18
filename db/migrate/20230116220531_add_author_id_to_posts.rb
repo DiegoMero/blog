df32ca00ec94cf5dac28b995374e55a862b115b8
Diego Mero <diegomero_1999@hotmail.com>
2023-01-17 23:56:04 -0500

    Fix snake_case instead of CamelCase columns name

--- db/migrate/20230116220531_add_author_id_to_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthorIdToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :authorId, :integer
-    add_index :posts, :authorId
-    add_foreign_key :posts, :users, column: :authorId
-  end
-end
