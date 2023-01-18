43bc13fe8e190b3555d50e063a2e4408bb91e0b7
Diego Mero <diegomero_1999@hotmail.com>
2023-01-18 00:32:53 -0500

    Improve custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User'
 
-  def update_posts_counter
-    user.update(postsCounter: user.posts.count)
-  end
-
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    author.increment!(:postsCounter)
   end
 end
