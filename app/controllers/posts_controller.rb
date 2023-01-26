9c3f24b078e0d81ed84d0caa423ff2599da6c57a
Diego Mero <diegomero_1999@hotmail.com>
2023-01-25 19:33:39 -0500

    Add form to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(@post.author_id)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(form_params.merge(author: current_user, likesCounter: 0, commentsCounter: 0))
+
+    if @post.save
+      redirect_to user_posts_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:post).permit(:title, :text)
+  end
 end
