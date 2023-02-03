27696bc4d0ec91c38cb286b9bf09ea38f16f0cbc
Diego Mero <diegomero_1999@hotmail.com>
2023-02-03 12:23:11 -0500

    Fix linter errors

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,11 +5,13 @@ class Api::CommentsController < ApplicationController
 
     render json: @comments
   end
-  
+
   def create
     @comment = Comment.new(params.require(:comment).permit(:text))
     @comment.post = Post.find(params[:post_id])
-    @comment.author = current_user    if @comment.save
+    @comment.author = current_user
+
+    if @comment.save
       render json: @comment
     else
       render json: @comment.errors
